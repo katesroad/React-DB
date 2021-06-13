1c4263d2c3f1b6d05bb3f06263d3ed930eaeb445
Kate <test@gmail.com>
2021-06-13 15:19:03 -0400

    react performance notes

---
+++ Performance/src/screens/index.tsx
@@ -0,0 +1,6 @@
+import { Layout } from 'components/common/Layout'
+import * as React from 'react'
+
+export default function HomeScreen() {
+  return <Layout name="home-scren" title="React Performance"></Layout>
+}
