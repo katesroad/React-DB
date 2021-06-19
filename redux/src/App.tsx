ff4f7623d5bf450c4d77b68be245e58a88ac2636
Kate <test@gmail.com>
2021-06-18 20:02:26 -0400

    updated App component

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -3,36 +3,38 @@ import Voter from 'components/Voter'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'store'
 import { getUser } from 'store/features/user'
+import './App.scss'
 
 function App() {
   const voters = ['John', 'Julian', 'Kate']
   const votes = useSelector((state: RootState) => state?.counter?.count || 0)
   const user = useSelector((state: RootState) => state?.user?.user)
-  const { isLoading, isSuccess } = useSelector((state: RootState) => state?.app)
+  const { isLoading, isIdle } = useSelector((state: RootState) => state?.app)
 
   const dispatch = useDispatch()
   React.useEffect(() => {
     dispatch(getUser())
   }, [dispatch])
 
+  if (isLoading || isIdle) {
+    return <p>Loading...</p>
+  }
+
   return (
-    <div>
+    <div className="App">
       <div className="heading">
         <h2>
           Votes for
-          {isLoading ? (
-            <span>loading...</span>
-          ) : isSuccess ? (
-            <span>{' ' + user?.lastName}</span>
-          ) : null}
+          <span>{' ' + user?.lastName}</span>
         </h2>
         <p>
-          <strong>Total votes:</strong> {votes} votes
+          <strong>Total votes:</strong>
+          <span aria-label="total-votes">{votes} votes</span>
         </p>
       </div>
       <ul>
         {voters.map((name) => (
-          <li key={name}>
+          <li key={name} aria-label={`voter-${name}`}>
             <Voter name={name} />
           </li>
         ))}
